675989b97fad2a3f172f298ce7e573796e23f349
Sergei <sergei@winback.com>
2025-04-10 17:05:29 +0200

    feat: add video URL for Pecs Fly exercise

---
+++ tier-maker-premium/assets/js/exercises.js
@@ -0,0 +1,113 @@
+const exercises = {
+  Jambes: [
+    {
+      name: "Leg curl cable machine",
+      video:
+        "https://media.musclewiki.com/media/uploads/videos/branded/male-Leg-lever-leg-curl-side.mp4#t=0.1",
+    },
+    {
+      name: "Extension câble machine",
+      video:
+        "https://media.musclewiki.com/media/uploads/videos/branded/male-Leg-lever-leg-extension-side.mp4",
+    },
+    {
+      name: "Squats Smith",
+      video:
+        "https://media.musclewiki.com/media/uploads/videos/branded/male-quads-smith-machine-squat-side.mp4",
+    },
+    {
+      name: "Bulgarian Split Squat",
+      video:
+        "https://media.musclewiki.com/media/uploads/videos/branded/male-quads-dumbbell-bulgarian-split-squat-side.mp4",
+    },
+    {
+      name: "Presse",
+      video:
+        "https://media.musclewiki.com/media/uploads/videos/branded/male-quads-leg-press-side.mp4",
+    },
+    { name: "Leg extension + RED band", video: "[Remplacer par URL]" },
+    { name: "Leg curl uni", video: "[Remplacer par URL]" },
+    { name: "Superset : Abductions / Adduction", video: "[Remplacer par URL]" },
+  ],
+  Épaules: [
+    {
+      name: "Oiseau à la poulie ou banc",
+      video:
+        "https://media.musclewiki.com/media/uploads/videos/branded/male-shoulders-cable-rear-delt-fly-side.mp4",
+    },
+    {
+      name: "Élévations latérales câble",
+      video:
+        "https://media.musclewiki.com/media/uploads/videos/branded/male-shoulders-cable-lateral-raise-side.mp4",
+    },
+    {
+      name: "Développé militaire",
+      video:
+        "https://media.musclewiki.com/media/uploads/videos/branded/male-shoulders-barbell-shoulder-press-front.mp4",
+    },
+  ],
+  Dos: [
+    { name: "Tirage horizontal trapèzes", video: "[Remplacer par URL]" },
+    { name: "Tirage horizontal unilatéral", video: "[Remplacer par URL]" },
+    {
+      name: "Tirage poulie haute",
+      video:
+        "https://media.musclewiki.com/media/uploads/videos/branded/male-back-lat-pulldown-front.mp4",
+    },
+    { name: "Relevé buste lombaires", video: "[Remplacer par URL]" },
+    {
+      name: "Tractions",
+      video:
+        "https://media.musclewiki.com/media/uploads/videos/branded/male-back-pull-up-front.mp4",
+    },
+  ],
+  Pectoraux: [
+    { name: "Machine inclinée", video: "[Remplacer par URL]" },
+    { name: "Smith incliné", video: "[Remplacer par URL]" },
+    {
+      name: "Pecs Fly",
+      video:
+        "https://media.musclewiki.com/media/uploads/videos/branded/male-Machine-machine-pec-fly-front.mp4#t=0.1",
+    },
+  ],
+  Biceps: [
+    {
+      name: "Curl biceps poulie neutre corde ou barre Z",
+      video: "[Remplacer par URL]",
+    },
+    {
+      name: "Dégressif biceps à l'échec curl assis pupitre",
+      video: "[Remplacer par URL]",
+    },
+  ],
+  Triceps: [
+    { name: "Extensions triceps barre", video: "[Remplacer par URL]" },
+    {
+      name: "Corde extension triceps banc incliné",
+      video: "[Remplacer par URL]",
+    },
+    { name: "Extension triceps dégressif en X", video: "[Remplacer par URL]" },
+  ],
+  Mollets: [
+    {
+      name: "Mollets debout",
+      video:
+        "https://media.musclewiki.com/media/uploads/videos/branded/male-calves-standing-calf-raise-side.mp4",
+    },
+    { name: "Mollets assis deep", video: "[Remplacer par URL]" },
+  ],
+  Trapezius: [
+    {
+      name: "Shrugs debout haltère",
+      video:
+        "https://media.musclewiki.com/media/uploads/videos/branded/male-traps-dumbbell-shrug-front.mp4",
+    },
+  ],
+  Abdos: [
+    {
+      name: "Abs circuit (bike/crunch/L/R/Plank)",
+      video: "[Remplacer par URL]",
+    },
+    { name: "Abdos à la barre", video: "[Remplacer par URL]" },
+  ],
+};
